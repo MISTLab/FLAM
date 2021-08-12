27a9b96a0303168f40a92a2fb92f5847483d178e
samarseneault <samuel.arseneault@polymtl.ca>
2021-08-12 18:06:37 -0400

    Experiment automation

---
+++ src/run.py
@@ -0,0 +1,27 @@
+"""
+This program's purpose is simply to run multiple ARGoS experiments in a headless way.
+To do so, it is necessary to comment out the <visualization> section 
+in the experiment file (./dora-mesh.argos).
+"""
+
+
+import subprocess
+
+
+NB_RUNS = 30
+
+
+def main() -> None:
+    command = ["argos3 -c dora-mesh.argos"]
+    for i in range(NB_RUNS):
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+        
+        for line in process.stdout:
+            print(line.decode())
+
+        process.wait()
+        print(f"Experiment #{i} finished with code {process.returncode}")
+
+
+if __name__ == "__main__":
+    main()
